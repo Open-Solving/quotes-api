b2a51b3826037369bfea916a8888dfcb52de49da
Aloïs Micard <lunamicard@gmail.com>
2020-01-03 00:50:23 +0100

    Finalize api implementation

--- api.go
+++ api.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"context"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/labstack/gommon/log"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"net/http"
+	"os"
+	"time"
 )
 
 type QuoteDto struct {
@@ -13,18 +21,56 @@ type QuoteDto struct {
 	Source string `json:"source"`
 }
 
+type QuoteEntity struct {
+	Id     primitive.ObjectID `bson:"_id"`
+	Text   string             `bson:"text"`
+	Source string             `bson:"source"`
+}
+
 func main() {
 	svr := echo.New()
 
-	svr.GET("/quotes", getQuotesHandler())
+	dbCtx, _ := context.WithTimeout(context.Background(), 2*time.Second)
+	dbClient, err := mongo.Connect(dbCtx, options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err != nil {
+		log.Fatalf("Unable to create database connection: %s", err)
+	}
+	if err := dbClient.Ping(dbCtx, readpref.Primary()); err != nil {
+		log.Fatalf("Unable to connect to database: %s", err)
+	}
+
+	svr.GET("/quotes", getQuotesHandler(dbClient))
 
 	svr.Use(middleware.CORS())
 
 	log.Fatal(svr.Start(":8080"))
 }
 
-func getQuotesHandler() echo.HandlerFunc {
+func getQuotesHandler(client *mongo.Client) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return c.JSON(http.StatusOK, []QuoteDto{{Id: "1", Text: "Test #1", Source: "Wikipedia"}, {Id: "2", Text: "Text #2", Source: "Nop"}})
+		collection := client.Database("quotes").Collection("quotes")
+		ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+
+		var quotes []QuoteEntity
+
+		cur, err := collection.Find(ctx, bson.M{})
+		if err != nil {
+			c.Logger().Errorf("Error while querying database")
+			return c.NoContent(http.StatusInternalServerError)
+		}
+
+		if err := cur.All(ctx, &quotes); err != nil {
+			c.Logger().Errorf("Error while querying database")
+			return c.NoContent(http.StatusInternalServerError)
+		}
+
+		quoteDtos := make([]QuoteDto, len(quotes))
+		for index, quote := range quotes {
+			quoteDtos[index].Id = quote.Id.Hex()
+			quoteDtos[index].Text = quote.Text
+			quoteDtos[index].Source = quote.Source
+		}
+
+		return c.JSON(http.StatusOK, quoteDtos)
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/creekorful/quotes-api
 
 go 1.13
 
-require github.com/labstack/echo/v4 v4.1.13 // indirect
+require (
+	github.com/DataDog/zstd v1.4.4 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/labstack/echo/v4 v4.1.13 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/xdg/stringprep v1.0.0 // indirect
+	go.mongodb.org/mongo-driver v1.2.0 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+)
